25c2f395c2e8694a1a1eb8cca167e20c7757584e
Isaiah <44090193+accurateisaiah@users.noreply.github.com>
2021-04-26 08:15:24 -0400

    fixed stuff

--- code/ui/Health.cs
+++ code/ui/Health.cs
@@ -1,32 +1,25 @@
-using Sandbox;
+﻿using Sandbox;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
 
-namespace Prophunt.UI.External
+public class Health : Panel
 {
-    public class Health : Panel
-    {
-        public Label Label;
+	public Label Label;
 
-        public Health()
-        {
-            object Health2 = Add.Label("💓", "icon");
+	public Health()
+	{
+          object Health2 = Add.Label("💓", "icon");
             Label = Add.Label("100", "value");
             if (player.Health) = 10f; // if the player health is under 10 change gui to broken heart
                     {
                 Health2 = Add.Label("💔", "icon";)
-            }
+	}
 
+	public override void Tick()
+	{
+		var player = Player.Local;
+		if ( player == null ) return;
 
-
-        }
-
-        public override void Tick()
-        {
-            var player = Player.Local;
-            if (player == null) return;
-
-            Label.Text = $"{player.Health:n0}";
-        }
-    }
+		Label.Text = $"{player.Health:n0}";
+	}
 }
