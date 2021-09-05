ab87c53a6afba90c739c034da478ef82b6cf1d7d
Shark-vil <shark_vil@mail.ru>
2021-09-06 02:15:48 +0300

    Implementing undo for - Entity

---
+++ code/modules/undo/objects/EntityUndo.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+
+public class EntityUndo : ICanUndo
+{
+	internal Entity entity { get; set; }
+
+	public EntityUndo( Entity entity ) => this.entity = entity;
+
+	public void DoUndo() => entity.Delete();
+
+	public bool IsValidUndo() => ( entity != null && entity.IsValid() );
+}
