0e626852649c5f54d7ce2cbab19e53d5157ab8bc
Shark-vil <shark_vil@mail.ru>
2021-09-06 02:15:53 +0300

    Implementing undo for - Particles

---
+++ code/modules/undo/objects/ParticlesUndo.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+
+public class ParticlesUndo : ICanUndo
+{
+	internal Particles particles { get; set; }
+
+	public ParticlesUndo( Particles particles ) => this.particles = particles;
+
+	public void DoUndo() => particles?.Destroy();
+
+	public bool IsValidUndo() => particles != null;
+}
