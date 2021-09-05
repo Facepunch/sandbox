a57bc27cb97400a034b6be48744ccbfae20956c3
Shark-vil <shark_vil@mail.ru>
2021-09-06 02:15:36 +0300

    Interface for working with mutable objects

---
+++ code/modules/undo/ICanUndo.cs
@@ -0,0 +1,13 @@
+﻿public interface ICanUndo
+{
+	/// <summary>
+	/// Calls the undo method.
+	/// </summary>
+	void DoUndo();
+
+	/// <summary>
+	/// Checks the validity of the data in the undo block.
+	/// </summary>
+	/// <returns>Returns <see langword="false"/> if the content of the parameters is violated in the block, otherwise <see langword="true"/>.</returns>
+	bool IsValidUndo();
+}
