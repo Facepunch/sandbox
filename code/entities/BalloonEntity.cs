5ec80d7e351489995340f49de923cf8494905665
Shark-vil <shark_vil@mail.ru>
2021-09-06 05:01:54 +0300

    Removing unnecessary interface implementation

--- code/entities/BalloonEntity.cs
+++ code/entities/BalloonEntity.cs
@@ -2,7 +2,7 @@
 using System;
 
 [Library( "ent_balloon", Title = "Balloon", Spawnable = true )]
-public partial class BalloonEntity : Prop, ICanUndo
+public partial class BalloonEntity : Prop
 {
 	private static float GravityScale => -0.2f;
 
@@ -36,12 +36,4 @@ public partial class BalloonEntity : Prop, ICanUndo
 
 		body.GravityScale = GravityScale;
 	}
-
-	/*
-	 * Implementation of the cancellation interface.
-	 * This interface allows you to add the entire entity to the entire undo block, without the need to create an intermediary.
-	 */
-	public void DoUndo() => Delete();
-
-	public bool IsValidUndo() => this.IsValid();
 }
