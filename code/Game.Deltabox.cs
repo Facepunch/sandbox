6fc6b30bdc3868150f5acc8bff2f109cc4eac520
Delta135 <135.delta@gmail.com>
2021-09-13 17:54:46 +0200

    Moved custom code to a separate file

---
+++ code/Game.Deltabox.cs
@@ -0,0 +1,47 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deltabox
+{
+	partial class SandboxGame : Game
+	{
+		[ServerCmd( "player_show", Help = "Hides the player" )]
+		public static void DrawPlayer()
+		{
+			var owner = ConsoleSystem.Caller?.Pawn;
+
+			setPlayerAlpha( 100, owner );
+		}
+
+		[ServerCmd( "player_hide", Help = "Shows the player" )]
+		public static void HidePlayer()
+		{
+			var owner = ConsoleSystem.Caller?.Pawn;
+
+			setPlayerAlpha( 0, owner );
+		}
+
+		private static void setPlayerAlpha( int alpha, Entity player )
+		{
+			ModelEntity playerModel = player as ModelEntity;
+			Color playerCrolor = playerModel.RenderColor;
+
+			playerCrolor.a = alpha;
+
+			for ( int i = 0; i < playerModel.Children.Count; i++ )
+			{
+				if ( playerModel.Children[i] is ModelEntity )
+				{
+					ModelEntity model = playerModel.Children[i] as ModelEntity;
+					model.RenderColor = playerCrolor;
+				}
+			}
+
+			playerModel.RenderColor = playerCrolor;
+		}
+	}
+}
--- code/Game.cs
+++ code/Game.cs
@@ -95,37 +95,6 @@ partial class SandboxGame : Game
 		}
 	}
 
-	[ServerCmd( "player_show", Help = "Hides the player")]
-	public static void DrawPlayer()
-	{
-		var owner = ConsoleSystem.Caller?.Pawn;
-
-		setPlayerAlpha( 100, owner );
-	}
-
-	[ServerCmd( "player_hide", Help = "Shows the player" )]
-	public static void HidePlayer()
-	{
-		var owner = ConsoleSystem.Caller?.Pawn;
-
-		setPlayerAlpha( 0, owner );
-	}
-
-	private static void setPlayerAlpha( int alpha, Entity player )
-	{
-		ModelEntity playerModel = player as ModelEntity;
-
-		for ( int i = 0; i < playerModel.Children.Count; i++ )
-		{
-			if ( playerModel.Children[i] is ModelEntity )
-			{
-				ModelEntity model = playerModel.Children[i] as ModelEntity;
-				model.RenderAlpha = alpha;
-			}
-		}
-
-		playerModel.RenderAlpha = alpha;
-	}
 	[ClientCmd( "debug_write" )]
 	public static void Write()
 	{
