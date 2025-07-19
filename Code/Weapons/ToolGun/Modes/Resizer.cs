9bea398a4eb1e5fc50ea0d9ed2e8c23e937db9ad
Layla <fortblox@gmail.com>
2025-07-19 07:05:00 +0100

    simple resizer tool, mostly to see if there's any issues with scaling physics in multiplayer

---
+++ Code/Weapons/ToolGun/Modes/Resizer.cs
@@ -0,0 +1,23 @@
+﻿
+[Icon( "🍄" )]
+[ClassName( "resizer" )]
+public class Resizer : ToolMode
+{
+	public override void OnControl()
+	{
+		var go = TraceSelect().GameObject;
+		if ( !go.IsValid() ) return;
+
+		if ( Input.Pressed( "attack1" ) ) Resize( go, 0.1f );
+		else if ( Input.Pressed( "attack2" ) ) Resize( go, -0.1f );
+	}
+
+	[Rpc.Host]
+	public void Resize( GameObject go, float size )
+	{
+		if ( !go.IsValid() ) return;
+
+		var scale = Vector3.Max( go.WorldScale + size, 0.01f );
+		go.WorldScale = scale;
+	}
+}
