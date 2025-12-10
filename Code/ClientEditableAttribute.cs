443690ef1e391d56ba1173985cda64adc03df8e5
Garry Newman <garrynewman@users.noreply.github.com>
2025-12-10 08:55:02 +0000

    Delete ClientEditableAttribute.cs

--- Code/ClientEditableAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace Sandbox;
-
-/// <summary>
-/// Indicates that this propery can be edited by the client, in a game like Sandbox Mode.
-/// </summary>
-[AttributeUsage( AttributeTargets.Property )]
-public sealed class ClientEditableAttribute : Attribute
-{
-}
