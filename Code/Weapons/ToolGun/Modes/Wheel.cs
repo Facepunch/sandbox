835cc144d5f904aee5870b606f112fa836efb473
Garry Newman <garrynewman@users.noreply.github.com>
2025-12-10 11:20:41 +0000

    Wheel suspension indicator, tries to face down, right click rotates

--- Code/Weapons/ToolGun/Modes/Wheel.cs
+++ Code/Weapons/ToolGun/Modes/Wheel.cs
@@ -6,6 +6,8 @@ public class Wheel : ToolMode
 {
 	Model wheelModel = Cloud.Model( "facepunch/tyre_with_rim" );
 
+	Vector3 _axis = Vector3.Right;
+
 	public override void OnControl()
 	{
 		base.OnControl();
@@ -17,8 +19,13 @@ public class Wheel : ToolMode
 		var modelBounds = wheelModel.Bounds;
 		var surfaceOffset = modelBounds.Size.y * 0.5f;
 
+		if ( Input.Pressed( "attack2" ) )
+		{
+			_axis = _axis == Vector3.Right ? Vector3.Up : Vector3.Right;
+		}
+
 		var placementTrans = new Transform( pos.Position + pos.Rotation.Forward * surfaceOffset );
-		placementTrans.Rotation = Rotation.LookAt( pos.Rotation.Forward, Vector3.Up ) * new Angles( 0, 90, 0 );
+		placementTrans.Rotation = Rotation.LookAt( pos.Rotation.Forward, pos.Rotation * _axis ) * new Angles( 0, 90, 0 );
 
 		if ( Input.Pressed( "attack1" ) )
 		{
@@ -27,6 +34,11 @@ public class Wheel : ToolMode
 		}
 
 		DebugOverlay.Model( wheelModel, transform: placementTrans, castShadows: true, color: Color.White.WithAlpha( 0.9f ) );
+
+		DebugOverlay.Line( new Line( placementTrans.Position, placementTrans.Position + placementTrans.Right * 5 ), Color.White );
+
+		var suspensionAxis = placementTrans.Forward * 20;
+		DebugOverlay.Line( new Line( placementTrans.Position - suspensionAxis, placementTrans.Position + suspensionAxis ), Color.Green );
 
 	}
 
@@ -51,6 +63,9 @@ public class Wheel : ToolMode
 		//var joint = jointGo.AddComponent<HingeJoint>();
 		var joint = jointGo.AddComponent<WheelJoint>();
 		joint.Attachment = Joint.AttachmentMode.Auto;
+		joint.EnableSuspension = true;
+		joint.EnableSuspensionLimit = true;
+		joint.SuspensionLimits = new Vector2( -32, 32 );
 		joint.Body = wheelAnchor;
 		joint.EnableCollision = false;
 
