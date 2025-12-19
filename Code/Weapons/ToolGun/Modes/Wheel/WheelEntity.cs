2fde2dfdf05647594f7ee9a81515c8f519a37f9f
Garry Newman <garrynewman@users.noreply.github.com>
2025-12-19 16:12:44 +0000

    WheelEntity is controllable!

--- Code/Weapons/ToolGun/Modes/Wheel/WheelEntity.cs
+++ Code/Weapons/ToolGun/Modes/Wheel/WheelEntity.cs
@@ -1,13 +1,28 @@
 ﻿public class WheelEntity : Component, IPlayerControllable
 {
-	[Property, Range( 0, 1 )]
+	[Property, Range( 0, 1 ), ClientEditable]
+	public bool Reversed { get; set; } = false;
+
+	[Property, Range( 0, 1 ), ClientEditable]
+	public float Speed { get; set; } = 0.5f;
+
+	[Property, Range( 0, 1 ), ClientEditable]
 	public float Power { get; set; } = 0.5f;
 
-	/// <summary>
-	/// While the client input is active we'll apply thrust
-	/// </summary>
 	[Property, Sync, ClientEditable]
-	public ClientInput Activate { get; set; }
+	public ClientInput Forward { get; set; }
+
+	[Property, Sync, ClientEditable]
+	public ClientInput Reverse { get; set; }
+
+	[Property, Sync, ClientEditable]
+	public ClientInput Break { get; set; }
+
+	[Property, Sync, ClientEditable]
+	public ClientInput TurnLeft { get; set; }
+
+	[Property, Sync, ClientEditable]
+	public ClientInput TurnRight { get; set; }
 
 	protected override void OnEnabled()
 	{
@@ -24,7 +39,49 @@
 
 	public void OnControl()
 	{
-		var analog = Activate.GetAnalog();
+		var joint = GetComponentInChildren<WheelJoint>();
+		if ( !joint.IsValid() ) return;
+
+		var forward = Forward.GetAnalog();
+		var reverse = Reverse.GetAnalog();
+		var speed = (forward - reverse).Clamp( -1, 1 );
+
+		if ( Break.GetAnalog() > 0.1f )
+		{
+			joint.EnableSpinMotor = true;
+			joint.SpinMotorSpeed = 0;
+			joint.MaxSpinTorque = 500000 * Power;
+		}
+		else if ( speed.AlmostEqual( 0.0f ) )
+		{
+			joint.EnableSpinMotor = false;
+		}
+		else
+		{
+			if ( Reversed ) speed = -speed;
+
+			joint.EnableSpinMotor = true;
+			joint.SpinMotorSpeed = -2000 * speed * Speed;
+			joint.MaxSpinTorque = 200000 * Power;
+		}
+
+		var left = TurnLeft.GetAnalog();
+		var right = TurnRight.GetAnalog();
+		var dir = (right - left).Clamp( -1, 1 );
+
+		if ( !dir.AlmostEqual( 0.0f ) )
+		{
+			joint.EnableSteering = true;
+			joint.SteeringDampingRatio = 1.0f;
+			joint.MaxSteeringTorque = 500000;
+			joint.SteeringLimits = new Vector2( -45, 45 );
+			joint.TargetSteeringAngle = 30 * dir;
+		}
+		else
+		{
+			joint.TargetSteeringAngle = 0;
+		}
+
 	}
 }
 
