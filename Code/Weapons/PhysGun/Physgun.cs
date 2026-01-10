5b45753b5ef570f368ce8b9fbedd40f31324a355
Layla <fortblox@gmail.com>
2026-01-10 18:36:10 +0000

    fix physgun pull not calculating local offset from mass center correctly when object is scaled

--- Code/Weapons/PhysGun/Physgun.cs
+++ Code/Weapons/PhysGun/Physgun.cs
@@ -246,7 +246,6 @@ public partial class Physgun : BaseCarryable
 				{
 					Active = true,
 					Pulling = true,
-					LocalOffset = sh.Body.MassCenter,
 				};
 			}
 		}
@@ -409,16 +408,19 @@ public partial class Physgun : BaseCarryable
 		var bodyTransform = tr.Body.Transform.WithScale( go.WorldScale );
 
 		state.GameObject = go;
-		state.LocalOffset = bodyTransform.PointToLocal( tr.HitPosition );
 		state.LocalNormal = bodyTransform.NormalToLocal( tr.Normal );
 
 		if ( isPulling )
 		{
+			// Scale is built into mass center, remove it.
+			var bodyScale = new Transform( Vector3.Zero, Rotation.Identity, bodyTransform.Scale );
+			state.LocalOffset = bodyScale.PointToLocal( tr.Body.LocalMassCenter );
 			state.GrabDistance = 0;
 			state.GrabOffset = aim.Rotation.Inverse * bodyTransform.Rotation;
 		}
 		else
 		{
+			state.LocalOffset = bodyTransform.PointToLocal( tr.HitPosition );
 			state.GrabDistance = Vector3.DistanceBetween( aim.Position, tr.HitPosition );
 			state.GrabDistance = ClampGrabDistance( state.Body, tr.HitPosition, aim, state.GrabDistance );
 			state.GrabOffset = Rotation.FromYaw( yaw ).Inverse * bodyTransform.Rotation;
