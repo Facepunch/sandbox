1f0837ad7b37befc092bf35a550aa19c3a357d6c
Tony Ferguson <devultj@gmail.com>
2026-03-30 13:59:31 +0100

    Fix inventory pickup tooltip using the item prefab's name, not the display name

--- Code/Items/Pickups/InventoryPickup.cs
+++ Code/Items/Pickups/InventoryPickup.cs
@@ -11,7 +11,7 @@ public sealed class InventoryPickup : BasePickup, Component.IPressable
 	IPressable.Tooltip? IPressable.GetTooltip( IPressable.Event e )
 	{
 		if ( Items == null || Items.Count == 0 ) return null;
-		return new IPressable.Tooltip( "Pick up", "inventory_2", string.Join( ", ", Items.Select( i => i.Name.ToUpper() ) ) );
+		return new IPressable.Tooltip( "Pick up", "inventory_2", string.Join( ", ", Items.Select( i => (i.GetComponent<BaseCarryable>()?.DisplayName ?? i.Name).ToUpper() ) ) );
 	}
 
 	public bool Press( IPressable.Event e )
