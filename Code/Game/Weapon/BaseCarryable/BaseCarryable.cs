6efdb5a7b7057cc90bce0c6d5b3dd9610365c632
Joey <70974815+fisherjoey@users.noreply.github.com>
2026-04-27 09:59:03 -0600

    Fix 'Wether' typo in BaseCarryable doc comment (#224)

--- Code/Game/Weapon/BaseCarryable/BaseCarryable.cs
+++ Code/Game/Weapon/BaseCarryable/BaseCarryable.cs
@@ -51,7 +51,7 @@ public partial class BaseCarryable : Component, IKillIcon
 	public virtual string InventoryIconOverride => null;
 
 	/// <summary>
-	/// Wether this weapon should be avoided when determining an item to swap to
+	/// Whether this weapon should be avoided when determining an item to swap to
 	/// </summary>
 	public virtual bool ShouldAvoid => false;
 
