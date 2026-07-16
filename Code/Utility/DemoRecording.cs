d301dbc038c11f7731bc458d18d496074986fb73
James King <james.king@facepunchstudios.com>
2026-07-16 16:02:48 +0200

    Can record view models now

--- Code/Utility/DemoRecording.cs
+++ Code/Utility/DemoRecording.cs
@@ -9,7 +9,6 @@ internal static class DemoRecording
 	public static MovieRecorderOptions BuildMovieRecorderOptions( MovieRecorderOptions options )
 	{
 		return options
-			.WithFilter( x => !x.Tags.Has( "viewmodel" ) )
 			.WithFilter( x => x.PrefabInstanceSource?.StartsWith( "prefabs/surface/" ) is not true );
 	}
 }
