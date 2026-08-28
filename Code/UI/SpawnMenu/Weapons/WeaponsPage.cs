1841941fc4c16b3e227ca7667d83a53499be7fd7
Tony Ferguson <devultj@gmail.com>
2026-08-28 15:59:52 +0100

    Fixed weapon tab not querying cloud results

--- Code/UI/SpawnMenu/Weapons/WeaponsPage.cs
+++ Code/UI/SpawnMenu/Weapons/WeaponsPage.cs
@@ -4,9 +4,9 @@ public class WeaponsPage : BaseSpawnMenu
 	protected override void Rebuild()
 	{
 		AddHeader( "#spawnmenu.section.categories" );
-		AddOption( "📦", "#spawnmenu.entity.all", () => new EntityListCloud { LocalCategory = "Weapon", Query = "category:weapon" } );
-		AddOption( "🗡️", "#spawnmenu.weapon.melee", () => new EntityListCloud { LocalCategory = "Weapon/Melee", Query = "category:weapon/melee" } );
-		AddOption( "🛠️", "#spawnmenu.weapon.utility", () => new EntityListCloud { LocalCategory = "Weapon/Utility", Query = "category:weapon/utility" } );
-		AddOption( "🔫", "#spawnmenu.weapon.weapons", () => new EntityListCloud { LocalCategory = "Weapon", IncludeLocalSubcategories = false, Query = "category:weapon" } );
+		AddOption( "📦", "#spawnmenu.entity.all", () => new EntityListCloud { LocalCategory = "Weapon", Query = "+weapon" } );
+		AddOption( "🗡️", "#spawnmenu.weapon.melee", () => new EntityListCloud { LocalCategory = "Weapon/Melee", Query = "+weapon +melee" } );
+		AddOption( "🛠️", "#spawnmenu.weapon.utility", () => new EntityListCloud { LocalCategory = "Weapon/Utility", Query = "+weapon +utility" } );
+		AddOption( "🔫", "#spawnmenu.weapon.weapons", () => new EntityListCloud { LocalCategory = "Weapon", IncludeLocalSubcategories = false, Query = "+weapon +ranged" } );
 	}
 }
