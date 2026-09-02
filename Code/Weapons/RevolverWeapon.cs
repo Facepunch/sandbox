6147eae874eac8c1cdd1db11b79bcd6a34aec241
Tony Ferguson <devultj@gmail.com>
2026-09-02 14:00:39 +0100

    add revolver

---
+++ Code/Weapons/RevolverWeapon.cs
@@ -0,0 +1,19 @@
+using Sandbox.Rendering;
+
+[Title( "Revolver" )]
+public sealed class RevolverWeapon : IronSightsWeapon
+{
+	protected override bool WantsPrimaryAttack()
+	{
+		return Input.Pressed( "attack1" );
+	}
+
+	public override void DrawCrosshair( HudPainter hud, Vector2 center )
+	{
+		var color = !HasPrimaryAmmo() || IsReloading || NextPrimaryFire > 0 ? CrosshairNoShoot : CrosshairCanShoot;
+
+		hud.SetBlendMode( BlendMode.Normal );
+		hud.DrawCircle( center, 5, Color.Black );
+		hud.DrawCircle( center, 3, color );
+	}
+}
